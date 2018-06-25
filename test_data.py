bd6d976e8c3838be1daa0ebf7803386248611eb1
guoyixiang <guo_yixiang@163.com>
2018-06-25 23:54:25 +0800

    Initial commit

---
+++ test_data.py
@@ -0,0 +1,16 @@
+class TestData():
+    """class for 1 test result"""
+
+    def __init__(self):
+        """initial class with
+        test_name, string, must
+        test_result_str, string, opt
+        test_number, long, opt
+        test_result_dig_status, bool
+        test_result_dig, double, opt
+        test_result_dig_unit, string, opt
+        test_result_dig_show_scale, double, opt
+        test_result_dig_precision, double, opt
+
+        test
+        """
